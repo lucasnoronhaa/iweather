16ed2544953a3474782f00b4c189494679f8cbf5
lucasNoronha <lucasnoronha23@hotmail.com>
2025-09-06 14:44:07 -0300

    cards

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,10 @@
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Ionicons } from '@expo/vector-icons';
+import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
-import { FlatList } from "react-native-gesture-handler";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+//Dados mockados
 
 const forcecastMock = [
   {id: '1', day: 'Seg', temp: '28º', },
@@ -10,6 +12,16 @@ const forcecastMock = [
   {id: '3', day: 'Qua', temp: '26º', },
   {id: '4', day: 'Qui', temp: '24º', },
 ];
+
+const forecast = [
+  { day: 'Seg', temp: 28, icon: 'sunny' },
+  { day: 'Ter', temp: 30, icon: 'partly-sunny' },
+  { day: 'Qua', temp: 26, icon: 'cloudy' },
+  { day: 'Qui', temp: 24, icon: 'rainy' },
+  { day: 'Sex', temp: 27, icon: 'sunny' },
+  { day: 'Sáb', temp: 29, icon: 'partly-sunny' },
+  { day: 'Dom', temp: 25, icon: 'cloudy' },
+]
 
 export default function App() {
   return (
@@ -25,22 +37,21 @@ export default function App() {
       <View style={styles.main}>
         <Text style={styles.temp}>28º</Text>
         <Text style={styles.condition}>Ensolarado</Text>
+        <Ionicons name="sunny" size={80} color="yellow" />
       </View>
 
-      {/*Lista de previsão*/}
-      <View style={styles.forecast}>
-        <FlatList
-        horizontal
-        data={forcecastMock}
-        keyExtractor={(item) => item.id}
-        renderItem={({item}) => (
-          <View style={styles.card}>
+      {/*Previsão do Tempo - Cards*/}
+      <View style={styles.forecastContainer}>
+        {forecast.map((item, index) => (
+          <View key={index} style={styles.card}>
+            <Ionicons name={item.icon} size={30} color="white" />
             <Text style={styles.day}>{item.day}</Text>
-            <Text style={styles.tempSmall}>{item.temp}</Text>
+            <Text style={styles.tempSmall}>{item.temp}º</Text>
+          </View>
+          ))}
       </View>
-        )}
-        />
-      </View>
+
+      {/*Status Bar */}
 
       <StatusBar style="light" />
     </LinearGradient>
@@ -49,20 +60,57 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, paddingTop: 50 },
-  header: { alignItems: "center", marginBottom: 40 },
-  city: { color: "#fff", fontSize: 25, fontWeight: "700" },
-  date: { color: "#fff", fontSize: 14, marginTop: 4 },
-  main: { flex: 1, justifyContent: "center", alignItems: "center" },
-  temp: { fontSize: 80, color: "#fff", fontWeight: "200" },
-  condition: { fontSize: 20, color: "#eee" },
-  forecast: { paddingBottom: 40 },
-  card: {
-    backgroundColor: "rgba(255,255,255,0.2)",
-    padding: 16,
-    marginHorizontal: 8,
-    borderRadius: 12,
+  container: { 
+    flex: 1,
+    paddingTop: 50
   },
-  day: { color: "#fff", fontSize: 16, fontWeight: "600" },
+  header: { 
+    alignItems: "center",
+    marginBottom: 40
+  },
+  city: { color: "#fff",
+    fontSize: 25,
+    fontWeight: "700"
+  },
+  date: { color: "#fff",
+    fontSize: 14,
+    marginTop: 4
+  },
+  main: { flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  temp: { 
+    fontSize: 80,
+    color: "#fff",
+    fontWeight: "200",
+    justifyContent: "center",
+    marginTop: 20
+  },
+  condition: { fontSize: 20,
+    color: "#eee",
+    justifyContent: "center"
+  },
+  forecast: { 
+    paddingBottom: 40
+  },
+  day: { 
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "600"
+  },
   tempSmall: { color: "#fff", fontSize: 14 },
+  forecastContainer: { 
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 40 
+  },
+  card: {
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    padding: 15,
+    borderRadius: 15,
+    alignItems: 'center',
+    width: 70,
+  },
+  
 });
