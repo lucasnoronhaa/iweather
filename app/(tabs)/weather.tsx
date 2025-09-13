adcaeaab7fe6d2a9252bc0849d9073578101d076
lucasNoronha <lucasnoronha23@hotmail.com>
2025-09-13 17:25:15 -0300

    remover arquivos e scripts não utilizados, inicio do card de resumo e remoção da barra de navegação

--- app/(tabs)/weather.tsx
+++ app/(tabs)/weather.tsx
@@ -1,13 +1,33 @@
 import { View, Text, StyleSheet } from "react-native";
 import { useLocalSearchParams } from "expo-router";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function WeatherScreen() {
     const {city} = useLocalSearchParams();
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Previsão para:</Text>
-            <Text style={styles.city}>{city}</Text>
+            {/* Card de clima */}
+            <View style={styles.card}>
+                <Text style={styles.city}>{city}</Text>
+                <Text style={styles.date}>Segunda-feira, 15 de maio de 2023</Text>
+
+                    {/* Temperatura + Ícone */}
+                <View style={styles.row}>
+                    <Text style={styles.temp}>28ºC</Text>
+                    <MaterialCommunityIcons name="weather-night-partly-cloudy" size={64} color="#fff" />
+                </View>
+                <Text style={styles.condition}>Parcialmente nublado</Text>
+            </View>
+
+                {/* Card com outros detalhes */}
+            <View style={styles.detalhes}>
+                <Text style={styles.detailText}>Sensação térmica: 30ºC</Text>
+                <Text style={styles.detailText}>Probabilidade de chuva: 0%</Text>
+                <Text style={styles.detailText}>Velocidade do vento: 8 km/h</Text>
+                <Text style={styles.detailText}>Umidade do ar 40%</Text>
+                <Text style={styles.detailText}>Índice UV 5</Text>
+            </View>
         </View>
     );
 }
@@ -19,14 +39,52 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
     },
-    title: {
-        fontSize: 20,
-        color: "#fff"
+    card: {
+        width: "95%",
+        backgroundColor: "#1c1c24",
+        padding: 20,
+        borderRadius: 10,
+        alignItems: "flex-start"
     },
     city: {
         fontSize: 24,
         fontWeight: "bold",
-        color: "#4f8ef7",
-        marginTop: 10,
-    }
+        color: "#fff"
+    },
+    date: {
+        fontSize: 14,
+        color: "#aaa",
+        marginBottom: 10
+    },
+    row: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "100%",
+        marginVertical: 10
+    },
+    temp: {
+        fontSize: 48,
+        fontWeight: "bold",
+        color: "#fff"
+    },
+    condition: {
+        fontSize: 18,
+        color: "#aaa",
+        marginTop: 8
+    },
+    detalhes: {
+        marginTop: 8,
+        width: "95%",
+        backgroundColor: "#1c1c24",
+        padding: 20,
+        borderRadius: 10,
+        alignItems: "flex-start"
+    },
+    detailText: {
+        fontSize: 16,
+        color: "#aaa",
+        marginBottom: 4,
+        textAlign: "justify"
+    },
 });
