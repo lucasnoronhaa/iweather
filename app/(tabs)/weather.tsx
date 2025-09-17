4dba90bbe4b5694338de89bb747105664510e2a3
lucasNoronha <lucasnoronha23@hotmail.com>
2025-09-16 22:34:20 -0300

    ajuste no design

--- app/(tabs)/weather.tsx
+++ app/(tabs)/weather.tsx
@@ -1,9 +1,29 @@
-import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import { View, Text, StyleSheet, ImageBackground, FlatList } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function WeatherScreen() {
     const {city} = useLocalSearchParams();
+
+    type WeatherIconName =
+        | "weather-sunny"
+        | "weather-cloudy"
+        | "weather-rainy"
+        | "weather-night-partly-cloudy";
+
+    type ForecastItem = {
+        day: string;
+        temp: string;
+        icon: WeatherIconName;
+    };
+
+    const forecast: ForecastItem[] = [
+        {day: "Ter", temp: "28º / 20º", icon: "weather-sunny"},
+        {day: "Qua", temp: "26º / 19º", icon: "weather-cloudy"},
+        {day: "Qui", temp: "24º/ 18º", icon: "weather-rainy"},
+        {day: "Sex", temp: "25º/ 17º", icon: "weather-night-partly-cloudy"},
+        {day: "Sáb", temp: "26º/ 21º", icon: "weather-sunny"},
+    ];
 
     return (
         <View style={styles.container}>
@@ -25,21 +45,48 @@ export default function WeatherScreen() {
             </ImageBackground>
 
                 {/* Card com outros detalhes */}
-            <View style={styles.detalhes}>
-                <Text style={styles.detailText}>Sensação térmica: 30ºC</Text>
-                <Text style={styles.detailText}>Probabilidade de chuva: 0%</Text>
-                <Text style={styles.detailText}>Velocidade do vento: 8 km/h</Text>
-                <Text style={styles.detailText}>Umidade do ar 40%</Text>
-                <Text style={styles.detailText}>Índice UV 5</Text>
+            <View style={styles.detailsRow}>
+                <View style={styles.detailCard}>
+                    <MaterialCommunityIcons name="thermometer" size={20} color="#fff" />
+                    <Text style={styles.detailText}>Sensação térmica: 30ºC</Text>
+                </View>
+                    
+                <View style={styles.detailCard}>
+                    <MaterialCommunityIcons name="weather-rainy" size={20} color="#fff" />
+                    <Text style={styles.detailText}>Probabilidade de chuva: 0%</Text>
+                </View>
+
+                <View style={styles.detailCard}>
+                    <MaterialCommunityIcons name="weather-windy" size={20} color="#fff" />
+                    <Text style={styles.detailText}>Velocidade do vento: 8 km/h</Text>
+                </View>
+
+                <View style={styles.detailCard}>
+                    <MaterialCommunityIcons name="water-percent" size={20} color="#fff" />
+                    <Text style={styles.detailText}>Umidade do ar 40%</Text>
+                </View>
+
+                <View style={styles.detailCard}>
+                    <MaterialCommunityIcons name="white-balance-sunny" size={20} color="#fff" />
+                    <Text style={styles.detailText}>Índice UV 5</Text>
+                </View>
             </View>
 
                 {/* Próximos dias */}
             <View style={styles.nextDays}>
-                <Text style={styles.detailText}>Sensação térmica: 30ºC</Text>
-                <Text style={styles.detailText}>Probabilidade de chuva: 0%</Text>
-                <Text style={styles.detailText}>Velocidade do vento: 8 km/h</Text>
-                <Text style={styles.detailText}>Umidade do ar 40%</Text>
-                <Text style={styles.detailText}>Índice UV 5</Text>
+                <FlatList
+                    data={forecast}
+                    keyExtractor={(item) => item.day}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    renderItem={({ item }) => (
+                        <View style={styles.dayCard}>
+                            <Text style={styles.dayText}>{item.day}</Text>
+                            <MaterialCommunityIcons name={item.icon} size={35} color="#fff" />
+                            <Text style={styles.dayText}>{item.temp}</Text>
+                        </View>
+                    )}
+                />
             </View>
         </View>
     );
@@ -49,8 +96,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#13131a",
-        justifyContent: "center",
+        paddingVertical: 20,
         alignItems: "center",
+        justifyContent: "flex-start",
+        marginTop: 30,
     },
     card: {
         width: "95%",
@@ -86,26 +135,48 @@ const styles = StyleSheet.create({
         color: "#aaa",
         marginTop: 8
     },
-    detalhes: {
-        marginTop: 8,
+    detailsRow: {
         width: "95%",
+        marginTop: 12
+    },
+    detailCard: {
+        flexDirection: "row",
+        alignItems: "center",
         backgroundColor: "#1c1c24",
+        marginVertical: 6,
         padding: 20,
-        borderRadius: 10,
-        alignItems: "flex-start"
+        borderRadius: 14,
     },
     detailText: {
-        fontSize: 16,
-        color: "#aaa",
-        marginBottom: 4,
-        textAlign: "justify"
+        color: "#ddd",
+        marginLeft: 6,
+        fontSize: 12,
     },
     nextDays: {
-        marginTop: 8,
-        width: "95%",
+        marginTop: 20,
+        paddingLeft: 10,
+        width: "100%",
+    },
+    dayCard: {
         backgroundColor: "#1c1c24",
-        padding: 20,
         borderRadius: 10,
-        alignItems: "flex-start"
+        paddingVertical: 14,
+        paddingHorizontal: 18,
+        alignItems: "center",
+        justifyContent: "center",
+        marginRight: 12,
+        width: 80,
+    },
+    dayText: {
+        color: "#fff",
+        fontSize: 14,
+        textAlign: "center",
+        padding: 4,
+        marginBottom: 6
+    },
+    tempText: {
+        color: "#aaa",
+        fontSize: 12,
+        marginTop: 6
     },
 });
