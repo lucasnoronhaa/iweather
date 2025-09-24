aef08316a70d97c20cc58bc0568bfe67383d9679
Lucas Noronha Alves <lucasnoronha23@hotmail.com>
2025-09-23 23:46:32 -0300

    inicio de implementação de API

--- app/(tabs)/weather.tsx
+++ app/(tabs)/weather.tsx
@@ -1,9 +1,33 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useLocalSearchParams } from "expo-router";
-import { FlatList, ImageBackground, StyleSheet, Text, View } from "react-native";
+import { FlatList, ImageBackground, StyleSheet, Text, View, ActivityIndicator  } from "react-native";
+import { useState, useEffect } from 'react';
+import { getWeatherByCity } from '../../scripts/weatherApi';
 
 export default function WeatherScreen() {
     const {city} = useLocalSearchParams();
+
+    const [weatherData, setWeatherData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+    const loadWeatherData = async () => {
+        if (!city) return; // Não faz nada se não tiver uma cidade
+
+        try {
+            setLoading(true); // Avisa que estamos começando a carregar
+            const data = await getWeatherByCity(city as string);
+            setWeatherData(data);
+        } catch (err) {
+            setError(err.message); // Salva a mensagem de erro se a busca falhar
+        } finally {
+            setLoading(false); // Avisa que terminamos de carregar (com sucesso ou erro)
+        }
+    };
+
+    loadWeatherData();
+}, [city]); // Este array faz o código rodar novamente se a cidade mudar
 
     type WeatherIconName =
         | "weather-sunny"
@@ -26,6 +50,22 @@ export default function WeatherScreen() {
         {day: "Sáb", max: "32°", min: "22°", icon: "weather-sunny"},
     ];
 
+    if (loading) {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator size="large" color="#8FB2F5" />
+            </View>
+        );
+    }
+
+    if (error) {
+        return (
+            <View style={styles.container}>
+                <Text style={{ color: 'red', fontSize: 18 }}>{error}</Text>
+            </View>
+        );
+    }
+
     return (
         <View style={styles.container}>
             
@@ -35,15 +75,15 @@ export default function WeatherScreen() {
                 style={styles.card}
                 imageStyle={{ borderRadius: 20, resizeMode: 'cover' }}
             >
-                <Text style={styles.city}>{city}</Text>
+                <Text style={styles.city}>{wheaterData?.name}, {weatherData?.sys.country}</Text>
                 <Text style={styles.date}>Segunda-feira, 15 de maio de 2023</Text>
 
                 {/* Temperatura + Ícone */}
                 <View style={styles.row}>
                     <View>
-                        <Text style={styles.temp}>28ºC</Text>
-                        <Text style={styles.tempMinMax}>26ºC / 32ºC</Text>
-                        <Text style={styles.condition}>Poucas nuvens</Text>
+                        <Text style={styles.temp}>{Math.round(weatherData?.main?.temp)}</Text>
+                        <Text style={styles.tempMinMax}>{Math.round(weatherData?.main?.temp_min)}C° / {Math.round(weatherData?.main?.temp_max)}C°</Text>
+                        <Text style={styles.condition}>{weatherData?.weather[0]?.description}</Text>
                     </View>
 
                     <MaterialCommunityIcons
@@ -62,7 +102,7 @@ export default function WeatherScreen() {
                 <View style={styles.detailRow}>
                     <MaterialCommunityIcons name="thermometer" size={20} color="#aaa" />
                     <Text style={styles.detailLabel}>Sensação térmica</Text>
-                    <Text style={styles.detailValue}>30ºC</Text>
+                    <Text style={styles.detailValue}>{Math.round(weatherData?.main?.feels_like)}C°</Text>
                 </View>
                     
                 <View style={styles.detailRow}>
@@ -74,13 +114,13 @@ export default function WeatherScreen() {
                 <View style={styles.detailRow}>
                     <MaterialCommunityIcons name="weather-windy" size={20} color="#aaa" />
                     <Text style={styles.detailLabel}>Velocidade do vento</Text>
-                    <Text style={styles.detailValue}>15 km/h</Text>
+                    <Text style={styles.detailValue}>{weatherData?.wind?.speed} km/h</Text>
                 </View>
 
                 <View style={styles.detailRow}>
                     <MaterialCommunityIcons name="water-percent" size={20} color="#aaa" />
                     <Text style={styles.detailLabel}>Umidade do ar</Text>
-                    <Text style={styles.detailValue}>60%</Text>
+                    <Text style={styles.detailValue}>{weatherData?.main?.humidity}%</Text>
                 </View>
 
                 <View style={styles.detailRowUv}>
